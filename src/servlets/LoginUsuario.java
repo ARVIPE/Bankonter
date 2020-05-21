b02c9995ff3f88ce7290f3c2a619cc39518637cb
anadoblado <anita_doblado@hotmail.com>
2020-05-21 12:10:22 +0200

    login

--- src/servlets/LoginUsuario.java
+++ src/servlets/LoginUsuario.java
@@ -20,26 +20,59 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @WebServlet("/LoginUsuario")
 public class LoginUsuario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+     
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
 	public LoginUsuario () {
 		super();
 	}
 	
 
 	/**
+	 * Obtengo un JSON tipo  
+	 *    {"usuOrEmail": xxxxx, "pass": xxxxx}
+	 * Hago una b�squeda de ese usuario en la BBDD, si lo encuentro lo devuelvo en formato JSON
+	 * Si el usuario no se encuentra se debe responder de una manera adecuada
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode rootNode = mapper.readTree(request.getInputStream());
-		String userOrEmail = rootNode.path("usuarioOMail").asText();
-		String password = rootNode.path("pass").asText();
 		
-		System.out.println("Usuario: " + userOrEmail + " - Password: " + password);
+		Usuario u= null;
+		String userOrEmail = null;
+		String password = null;
 		
-		Usuario u = UsuarioControlador.getControlador().find(1);
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode rootNode = mapper.readTree(request.getInputStream());
+		    userOrEmail = rootNode.path("usuarioOMail").asText();
+			password = rootNode.path("pass").asText();
+			
+			// Busco al usuario en la BBDD
+			u = UsuarioControlador.getControlador().findByPasswordAndUsernameOrMail(userOrEmail, userOrEmail, password);
+			// Si no encuntro al usuario, informo
+			if (u == null) {
+				System.out.println("No se puede localizar al usuario " + userOrEmail + " con password " + password);
+			}
+			// Si encuentro al usuario establezco su imagen a null, porque en este caso no quiero que dicha imagen viaje en el JSON de salida
+			else {
+				u.setImagen(null);
+			}
+			
+		}
+		catch (Exception ex){
+			//Ocurrio una excepción en el acceso a datos o un error que no nos impide acceder a los campos del JSON
+			ex.printStackTrace();
+		}
+		
+		
+		
+		//System.out.println("Usuario: " + userOrEmail + " - Password: " + password);
+		
+		//Usuario u = UsuarioControlador.getControlador().find(1);
 		
 		// Creo el JSON de salida y lo devuelvo al cliente
+		ObjectMapper mapper = new ObjectMapper();
 		response.getWriter().println(mapper.writeValueAsString(u));
 
 	}
