d5ca13b238f6448993882acaaa150792c8f00442
amedrod609 <antmedrod@gmail.com>
2020-05-25 12:07:41 +0200

    Added scripts to login.html

--- WebContent/web/js/jquery-utils.js
+++ WebContent/web/js/jquery-utils.js
@@ -1,5 +1,4 @@
 
-
 // Siempre que la página esté completamente cargada se aplica lo siguiente
 $(document).ready(function () {
     /**
@@ -34,6 +33,16 @@ function getRequestParameters() {
     return requestParams;
 };
 
+
+/**
+ * Permite establecer un prefijo que se usará en el acceso a todos los servlets accedidos por
+ * el método "sendJsonRequest"
+ */
+var servletsPrefix = "";
+function setServletsPrefix (newServletsPrefix) {
+	servletsPrefix = newServletsPrefix;
+}
+
 /**
  * Hace una llamada a un request JSON, pasando un json de envío y recibiendo una respuesta.
  * Además también se encarga de activar la ventana modal de "cargando" y de desactivarla cuando
@@ -44,7 +53,7 @@ function getRequestParameters() {
  * @param {*} errorFunction 
  */
 function sendJsonRequest(url, jsonSendingData, successFunction, errorFunction) {
-    $('#infoModal').fadeIn('100');
+    url = servletsPrefix + url;
     $.ajax(url, {
         data: jsonSendingData,
         contentType: 'application/json',
@@ -52,13 +61,22 @@ function sendJsonRequest(url, jsonSendingData, successFunction, errorFunction) {
         dataType: 'json',
         success: function (data, status) {
             successFunction(data, status);
-            $('#infoModal').fadeOut();
         },
         error: function (xhr, strError, exception) {
             if (errorFunction != null) {
-                errorFunction(strError);
+                var resumenError = "";
+                // Controlo si ha ocurrido un error en el xhr
+                if (xhr != null) {
+                    resumenError += "Error " + xhr.status + " accediendo a " + url;
+                }
+                // Completo la información del error
+                resumenError += "<br/>StrError: " + strError + "<br/>Exception: " + exception + " - Mensaje: " + exception.message;
+                // Introduzco la info del JSON enviado
+                resumenError += "<br/>JSON enviado: " + jsonSendingData;
+
+                // Envío el error a la función definida por el usuario
+                errorFunction(resumenError);
             }
-            $('#infoModal').fadeOut();
         }
     });
 }
@@ -66,6 +84,7 @@ function sendJsonRequest(url, jsonSendingData, successFunction, errorFunction) {
 
 // Variables para especificar el tipo de alerta que se desea mostrar
 var ALERT_INFO = 1;
+var ALERT_DANGER = 2;
 /**
  * Muestra alertas en la parte superior de la página. Si hubiera alertas previas, las elimina con 
  * una suave animación.
@@ -73,7 +92,7 @@ var ALERT_INFO = 1;
  * @param {*} shortMsg 
  * @param {*} msg 
  */
-function showAlertMessage(type, shortMsg, msg) {
+function showAlertMessage(container, type, shortMsg, msg) {
     // Si existen alertas en pantalla, las elimino
     var millisToDelayNewMsg = 0;
     if ($(".alert") != null) {
@@ -89,6 +108,10 @@ function showAlertMessage(type, shortMsg, msg) {
     if (type == ALERT_INFO) {
         htmlToPrepend += ' alert-success ';
     }
+    else if (type == ALERT_DANGER) {
+        htmlToPrepend += ' alert-danger ';
+    }
+
     htmlToPrepend += 'alert-dismissible fade show" role="alert">';
     if (shortMsg != null) {
         htmlToPrepend += '<strong>' + shortMsg + '</strong> ';
@@ -100,10 +123,33 @@ function showAlertMessage(type, shortMsg, msg) {
 
     // Después de construir el html, finalmente lo muestro con una animación
     setTimeout(function () {
-        $("#containerPrincipal").prepend(htmlToPrepend);
+        container.prepend(htmlToPrepend);
     }, millisToDelayNewMsg);
     $(".alert").fadeIn(300);
 }
+
+
+
+
+/**
+ * En esta función recibimos un elemento visible de la página y le acoplamos un
+ * spinner, un elemento que tiene una animación para hacer visible al usuario
+ * que estamos, por ejemplo, procesando una petición
+ * @param {*} containerToPutWaitingIcon 
+ */
+function insertWaitingIcon (containerToPutWaitingIcon) {
+    containerToPutWaitingIcon.addClass("spinner-border spinner-border-sm");
+}
+
+/**
+ * En la función anterior agregamos una animación a un elemento, en esta función
+ * eliminamos ese elemento.
+ * @param {*} containerToPutWaitingIcon 
+ */
+function removeWaitingIcon (containerToPutWaitingIcon) {
+    containerToPutWaitingIcon.removeClass("spinner-border spinner-border-sm");
+}
+
 
 
 /**
@@ -125,4 +171,63 @@ function getDataFromFileInput(fileInput, onFileLoadedFunction) {
     else {
         onFileLoadedFunction(null);
     }
+}
+
+
+
+
+
+// Todos los elmentos de un formulario que incorporen la clase "checkValidity" tendrán el comportamiento
+// de comprobar su validez cuando pierdan el foco.
+$(document).ready(function () {
+    $(".checkValidity").blur(function () {
+        checkInputFormValidity ($(this)); // Comprobamos la validez del elemento
+    })
+});
+
+// Expresiones regulares que podremos utilizar en cualquier momento
+const EMAIL_REGULAR_EXPRESION = /\S+@\S+\.\S+/; // Expresión regular para un email -  validity="email"
+const NO_EMPTY_REGULAR_EXPRESION = /.+/;        // Expresión regular para cadena no vacía -  validity="noEmpty" o validity=""
+
+function getRegularExpressionValidityFromElement(element) {
+    var description = element.attr("validity"); // Obtengo el valor del atributo "validity" del elemento
+    if (description == "email") {
+        return EMAIL_REGULAR_EXPRESION;
+    }
+    else { // Si ninguna de las expresiones regulares anteriores se aplica, aplicamos la de NO_EMPTY
+        return NO_EMPTY_REGULAR_EXPRESION;
+    }
+}
+
+/**
+ * Permite comprobar la validez en campos de formularios con respecto a una expresión regular
+ * @param {} inputFormElement 
+ * @param {*} regularExpression 
+ */
+function checkInputFormValidity (inputFormElement) {
+    // Para poder comprobar la validez de un elemento, este tiene que tener un atributo que indique el tipo de validez
+    // que necesita, se admiten varios valores. Estos valores se pueden ver en la función (este mismo fichero) getRegularExpressionValidityFromElement
+    var regularExpression = getRegularExpressionValidityFromElement($(this));
+    if (!regularExpression.test(inputFormElement.val())) { // Compruebo la validación 
+        inputFormElement.addClass("is-invalid"); // Incluir esta clase provaca un efecto visual en el elemento del formulario
+        return false;
+    }
+    else {
+        inputFormElement.removeClass("is-invalid");
+        return true;
+    }
+}
+
+/**
+ * Comprueba la validez de un formulario, pasando uno a uno por todos sus inputs que tengan la clase "checkValidity"
+ * @param {} form 
+ */
+function checkFormValidity (form) {
+    var formIsValid = true;
+    form.find('.checkValidity:input').each(function (){
+        if (!checkInputFormValidity($(this))) {
+            formIsValid = false;
+        }
+    });
+    return formIsValid;
 }
