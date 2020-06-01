ffccc8cae7edd77d313f1e676892390e6f20cc64
anadoblado <anita_doblado@hotmail.com>
2020-06-01 20:33:31 +0200

    bbdd nueva empieza v06

--- BBDD/bankonter_bbdd.sql
+++ BBDD/bankonter_bbdd.sql
@@ -18,6 +18,38 @@ USE `bankonter`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `contrato`
+--
+
+DROP TABLE IF EXISTS `contrato`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contrato` (
+  `id` int NOT NULL,
+  `descriptor` varchar(45) DEFAULT NULL,
+  `saldo` float DEFAULT NULL,
+  `limite` float DEFAULT NULL,
+  `idTipoContrato` int DEFAULT NULL,
+  `idUsuario` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_contrato_tipoContrato1_idx` (`idTipoContrato`),
+  KEY `fk_contrato_usuario1_idx` (`idUsuario`),
+  CONSTRAINT `fk_contrato_tipoContrato1` FOREIGN KEY (`idTipoContrato`) REFERENCES `tipocontrato` (`id`),
+  CONSTRAINT `fk_contrato_usuario1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contrato`
+--
+
+LOCK TABLES `contrato` WRITE;
+/*!40000 ALTER TABLE `contrato` DISABLE KEYS */;
+INSERT INTO `contrato` VALUES (1,'ES11 1111 1111 0000 0001',42453.9,0,1,1),(2,'ES11 1111 1111 0000 0002',7698.65,0,1,1),(3,'ES11 1111 1111 0000 0003',174223,0,1,1),(4,'ES11 1111 2222 0000 0001',234.65,0,2,1),(5,'ES11 1111 2222 0000 0002',654.87,3000,3,1),(6,'ES11 1111 3333 0000 0001',42452.3,0,4,1),(7,'ES11 1111 3333 0000 0002',7698.96,0,4,1);
+/*!40000 ALTER TABLE `contrato` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `imagen`
 --
 
@@ -62,6 +94,30 @@ CREATE TABLE `sequence` (
 LOCK TABLES `sequence` WRITE;
 /*!40000 ALTER TABLE `sequence` DISABLE KEYS */;
 /*!40000 ALTER TABLE `sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipocontrato`
+--
+
+DROP TABLE IF EXISTS `tipocontrato`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tipocontrato` (
+  `id` int NOT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipocontrato`
+--
+
+LOCK TABLES `tipocontrato` WRITE;
+/*!40000 ALTER TABLE `tipocontrato` DISABLE KEYS */;
+INSERT INTO `tipocontrato` VALUES (1,'Cuenta'),(2,'Tarjeta débito'),(3,'Tarjeta crédito'),(4,'Préstamo');
+/*!40000 ALTER TABLE `tipocontrato` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -113,4 +169,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-01 12:33:59
+-- Dump completed on 2020-06-01 20:32:19
